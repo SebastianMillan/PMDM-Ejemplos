6d65f2cd8e2098f803ce9f241ca530414797b288
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-09-25 13:37:10 +0200

    EvInicial DI-PMDM

---
+++ EvInicial/index.js
@@ -0,0 +1,48 @@
+$(document).ready(() => {
+
+    $(document).on('click', '#btnGener', () => {
+        var titulo = $('#titulo').val().toLowerCase();
+        var urlResult;
+        if (titulo == '') {
+            $('#titulo').addClass('is-invalid');
+        }
+        urlResult = titulo.split(' ').join('-');
+        $('#url').val(urlResult);
+    });
+
+    $(document).on('click', '#btnAcept', () => {
+        var titulo = $('#titulo').val();
+        var url = $('#url').val();
+        var descr = $('#descripcion').val();
+        var num1 = Math.floor(Math.random() * 10);
+        var num2 = Math.floor(Math.random() * 10);
+        var result = num1 + num2;
+        var respuestaNumber = $('#numberProtect').val();
+        $('.security').removeClass('visually-hidden')
+        $('#numberProtect').removeClass('is-invalid');
+
+        if (titulo == '') {
+            $('#titulo').addClass('is-invalid');
+        } else {
+            $('#titulo').removeClass('is-invalid');
+        }
+        if (url == '') {
+            $('#url').addClass('is-invalid');
+        } else {
+            $('#url').removeClass('is-invalid');
+        }
+        if (descr == '') {
+            $('#descripcion').addClass('is-invalid');
+        } else {
+            $('#descripcion').removeClass('is-invalid');
+        }
+        $('#preguntaNumber').text("¿Cuál es la suma entre " + num1 + " y " + num2 + "?");
+        if (result == respuestaNumber) {
+            $('#numberProtect').removeClass('is-invalid');
+            $('.security').addClass('visually-hidden');
+        } else {
+            $('#numberProtect').addClass('is-invalid');
+
+        }
+    });
+});
