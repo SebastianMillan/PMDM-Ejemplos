f6af6a197bfe4a12b163ef7f80426b1655214ab0
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-09-25 19:46:27 +0200

    Empezar curso OW JQuery

---
+++ OWCursoJQuery/ejemplo1.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+    $('#id');
+    $('.class');
+    $('input:required');
+    $('img:last-of-type');
+    $('td:first-child');
+    $('h1:hover');
+    $('a:visited')
+    $('input:nth-child(4)').css('background-color', 'red');
+    $('input:nth-child(4)').css('color', 'blue');
+    $('input:nth-child(4)').hide();
+    $('input:last-child').show();
+    //eq(index) -> The eq() method returns an element with a specific index number of the selected elements.
+    $(selector).eq(index);
+    $('.card img').eq(0).hide();
+    $('.card img').eq(0).show();
+
+
+
+});
