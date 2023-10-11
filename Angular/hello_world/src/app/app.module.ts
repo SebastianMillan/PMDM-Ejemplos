6a9328168aea4c15b47af1d0b560faf03f39365d
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-10-11 14:24:17 +0200

    Componentes Angular de CardCoches y CardRestaurante

--- Angular/hello_world/src/app/app.module.ts
+++ Angular/hello_world/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { RegisterScreenComponent } from './register-screen/register-screen.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardComponentComponent } from './card-component/card-component.component';
+import { CardRestauranteComponent } from './card-restaurante/card-restaurante.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegisterScreenComponent,
+    CardComponentComponent,
+    CardRestauranteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
