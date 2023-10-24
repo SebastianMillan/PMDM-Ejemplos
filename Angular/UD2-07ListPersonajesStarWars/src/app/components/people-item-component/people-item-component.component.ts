e9ed6e5552a90f099078a10c3808f7b146106926
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-10-24 18:14:23 +0200

    Ejercicio Star Wars U2-7 Personajes

---
+++ Angular/UD2-07ListPersonajesStarWars/src/app/components/people-item-component/people-item-component.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { People } from 'src/app/models/people.interface';
+
+@Component({
+  selector: 'app-people-item-component',
+  templateUrl: './people-item-component.component.html',
+  styleUrls: ['./people-item-component.component.css']
+})
+export class PeopleItemComponentComponent {
+  @Input() people!: People;
+  @Output() peopleClick = new EventEmitter<string>();
+  peopleId = this.people.url.split('/')[4];
+  peopleName = '';
+
+  constructor(private modalService: NgbModal) { }
+  
+  viewPeopleDetail(modal: any) {
+    //this.peopleClick.emit(this.peopleId);
+    this.peopleName = this.people.name;
+  }
+
+  getPeopleImg() {
+    return `https://starwars-visualguide.com/assets/img/characters/${this.peopleId}.jpg`
+  }
+}
