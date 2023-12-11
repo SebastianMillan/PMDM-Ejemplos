88b2fdb667e6eca8fc157ec196e11b47b0322155
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-12-11 22:26:31 +0100

    Empezar Curso Angular Firebase

---
+++ Angular17/AngularFireBaseCurso/src/environments/environment.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyD3vg3_t5inUX74ZhUxk-GIQVN40bFbKEA",
+        authDomain: "angularfirebasecurso-1afad.firebaseapp.com",
+        databaseURL: "https://angularfirebasecurso-1afad-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "angularfirebasecurso-1afad",
+        storageBucket: "angularfirebasecurso-1afad.appspot.com",
+        messagingSenderId: "786939493731",
+        appId: "1:786939493731:web:7d14da25958cf6ee7be756",
+        measurementId: "G-51KSDVTDL1"
+    }
+}
+
+// Initialize Firebase
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
